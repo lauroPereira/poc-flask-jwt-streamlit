653e1993dd3ea8b27631b9ea1a8878f7105d1921
lauroPereira <lauro.pereira@ampeli.com.br>
2025-01-21 16:39:29 -0300

    feat teacher and courses relationship

--- models/course_model.py
+++ models/course_model.py
@@ -2,11 +2,13 @@ from . import db
 from .base_model import BaseModel
 
 class Course(BaseModel):
-    __tablename__ = 'classes'
+    __tablename__ = 'courses'
 
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     descricao = db.Column(db.Text, nullable=True)
+    
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), nullable=True)
 
     def __repr__(self):
         return f"<Course {self.nome}>"
