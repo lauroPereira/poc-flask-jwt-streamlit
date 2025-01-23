794acda72f16003d2c5db2d752de687202ba4bee
lauroPereira <lauro.pereira@ampeli.com.br>
2025-01-22 22:16:40 -0300

    fix Students model

--- models/student_model.py
+++ models/student_model.py
@@ -1,7 +1,20 @@
 from . import db
+from .base_model import BaseModel
+from .student_course_model import students_courses
 
-students_courses = db.Table(
-    'students_courses',
-    db.Column('student_id', db.Integer, db.ForeignKey('students.id'), primary_key=True),
-    db.Column('course_id', db.Integer, db.ForeignKey('turmas.id'), primary_key=True)
-)
+class Student(BaseModel):
+    __tablename__ = 'students'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+
+    # Relação muitos-para-muitos com Course
+    courses = db.relationship(
+        "Course",
+        secondary=students_courses,
+        back_populates="students"
+    )
+
+    def __repr__(self):
+        return f"<Student {self.nome}>"
