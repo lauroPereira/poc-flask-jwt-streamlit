6aacaf2a5053d4120b7e5458ab1288abbbbd9b55
lauroPereira <lauro.pereira@ampeli.com.br>
2025-01-23 22:57:28 -0300

    fix m2m course to student relationship

--- models/course_model.py
+++ models/course_model.py
@@ -1,5 +1,6 @@
 from . import db
 from .base_model import BaseModel
+from .student_course_model import students_courses  # importa a tabela intermediária
 
 class Course(BaseModel):
     __tablename__ = 'courses'
@@ -9,6 +10,12 @@ class Course(BaseModel):
     descricao = db.Column(db.Text, nullable=True)
     
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), nullable=True)
+    
+    students = db.relationship(
+        "Student",                  # Nome da classe que vamos relacionar
+        secondary=students_courses, # tabela intermediária
+        back_populates="courses"    # propriedade do outro lado (em Student)
+    )
 
     def __repr__(self):
         return f"<Course {self.nome}>"
--- models/student_course_model.py
+++ models/student_course_model.py
@@ -3,5 +3,5 @@ from . import db
 students_courses = db.Table(
     'students_courses',
     db.Column('student_id', db.Integer, db.ForeignKey('students.id'), primary_key=True),
-    db.Column('course_id', db.Integer, db.ForeignKey('turmas.id'), primary_key=True)
+    db.Column('course_id', db.Integer, db.ForeignKey('courses.id'), primary_key=True)
 )
